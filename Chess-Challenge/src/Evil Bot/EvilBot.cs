58add7727b528271f16331e7dbad6a29036514f5
Wetti <timo.w08@gmail.com>
2023-07-29 01:58:31 +0200

    caching

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -57,107 +57,65 @@ namespace ChessChallenge.Example
     }
         */
 
-        // Piece values: null, pawn, knight, bishop, rook, queen, king
-        int[] pieceValues = { 0, 100, 300, 330, 500, 900, 10000 };
-        readonly int maxPieceSum;
+        //                     .  P    K    B    R    Q    K
+        int[] kPieceValues = { 0, 100, 300, 310, 500, 900, 10000 };
+        int kMassiveNum = 99999999;
 
-        int m_depth = 3;
-        int maxMillisPerTurn;
-
-        public EvilBot()
-        {
-            maxPieceSum = pieceValues[1] * 8 + pieceValues[2] * 2 + pieceValues[3] * 2 + pieceValues[4] * 2 + pieceValues[5] + pieceValues[6];
-        }
+        int mDepth;
+        Move mBestMove;
 
         public Move Think(Board board, Timer timer)
         {
-            int pieces = BitboardHelper.GetNumberOfSetBits(board.AllPiecesBitboard);
-            if (pieces < 15) m_depth = 4;
-            else if (pieces <= 10) m_depth = 5;
-            else if (pieces <= 8) m_depth = 6;
-            else if (pieces <= 6) m_depth = 8;
+            Move[] legalMoves = board.GetLegalMoves();
+            mDepth = 3;
 
-            maxMillisPerTurn = timer.MillisecondsRemaining / 10;
+            EvaluateBoardNegaMax(board, mDepth, -kMassiveNum, kMassiveNum, board.IsWhiteToMove ? 1 : -1);
 
-            List<Move> bestMoves = new List<Move>();
-            int bestScore = int.MinValue;
-            foreach (Move move in board.GetLegalMoves().OrderByDescending(m => pieceValues[(int)m.MovePieceType]))
+            return mBestMove;
+        }
+
+        int EvaluateBoardNegaMax(Board board, int depth, int alpha, int beta, int color)
+        {
+            Move[] legalMoves;
+
+            if (board.IsDraw())
+                return 0;
+
+            if (depth == 0 || (legalMoves = board.GetLegalMoves()).Length == 0)
+            {
+                // EVALUATE
+                int sum = 0;
+
+                if (board.IsInCheckmate())
+                    return -9999999;
+
+                for (int i = 0; ++i < 7;)
+                    sum += (board.GetPieceList((PieceType)i, true).Count - board.GetPieceList((PieceType)i, false).Count) * kPieceValues[i];
+                // EVALUATE
+
+                return color * sum;
+            }
+
+            // TREE SEARCH
+            int recordEval = int.MinValue;
+            foreach (Move move in legalMoves)
             {
                 board.MakeMove(move);
-                int score = CalcRecursive(board, timer, m_depth, 1);
+                int evaluation = -EvaluateBoardNegaMax(board, depth - 1, -beta, -alpha, -color);
                 board.UndoMove(move);
 
-                if (score > bestScore)
+                if (recordEval < evaluation)
                 {
-                    bestMoves.Clear();
-                    bestScore = score;
+                    recordEval = evaluation;
+                    if (depth == mDepth)
+                        mBestMove = move;
                 }
-                if (score >= bestScore) bestMoves.Add(move);
+                alpha = Math.Max(alpha, recordEval);
+                if (alpha >= beta) break;
             }
-            return SelectBestMove(board, bestMoves);
-        }
+            // TREE SEARCH
 
-        int EvalMaterial(Board board)
-        {
-            return board.GetAllPieceLists().Chunk(6).Select(chunk => chunk.Sum(list => list.Sum(p => pieceValues[(int)p.PieceType]))).Aggregate((white, black) => white - black);
-        }
-
-        int CalcRecursive(Board board, Timer timer, int maxDepth, int currentDepth)
-        {
-            if (board.IsInCheckmate()) return currentDepth % 2 == 0 ? -maxPieceSum : maxPieceSum;
-            if (board.IsInsufficientMaterial() || board.IsRepeatedPosition() || board.FiftyMoveCounter == 50 || board.IsDraw()) return 0;
-
-            int boardScore = (currentDepth % 2 == 0) ^ board.IsWhiteToMove ? -EvalMaterial(board) : EvalMaterial(board);
-            if (currentDepth >= maxDepth) return boardScore;
-
-            int bestScore = int.MinValue;
-            foreach (Move move in board.GetLegalMoves().OrderByDescending(m => pieceValues[(int)m.MovePieceType]))
-            {
-                board.MakeMove(move);
-                int score = (int)(CalcRecursive(board, timer, maxDepth, currentDepth + 1) * 0.9 + boardScore);
-                board.UndoMove(move);
-                if (score > bestScore) bestScore = score;
-                if (timer.MillisecondsElapsedThisTurn > maxMillisPerTurn) return bestScore;
-            }
-            return bestScore;
-        }
-
-        Move SelectBestMove(Board board, List<Move> moves)
-        {
-            Move bestMove = moves[new Random().Next(moves.Count)];
-            int bestValue = -100_000;
-
-            foreach (Move move in moves)
-            {
-                Square kingSquare = board.GetKingSquare(!board.IsWhiteToMove);
-                int promotionValue = pieceValues[(int)move.CapturePieceType];
-                int captureValue = pieceValues[(int)move.CapturePieceType];
-                int lostPieceValue = HighestLostPiece(board, move);
-                int progress = move.MovePieceType == PieceType.Pawn ? GetDistance(move.StartSquare, move.TargetSquare) : GetDistance(kingSquare, move.StartSquare) - GetDistance(kingSquare, move.TargetSquare);
-
-                int score = captureValue - lostPieceValue + promotionValue + progress;
-                if (score > bestValue)
-                {
-                    bestMove = move;
-                    bestValue = score;
-                }
-            }
-
-            return bestMove;
-        }
-
-        int HighestLostPiece(Board board, Move move)
-        {
-            board.MakeMove(move);
-            int maxValue = board.GetAllPieceLists().Chunk(6).ElementAt(board.IsWhiteToMove ? 1 : 0).SelectMany(list => list)
-                .Where(p => board.GetLegalMoves(true).Select(m => m.TargetSquare).Contains(p.Square)).Where(p => !board.SquareIsAttackedByOpponent(p.Square)).Select(p => pieceValues[(int)p.PieceType]).DefaultIfEmpty(0).Max();
-            board.UndoMove(move);
-            return maxValue;
-        }
-
-        int GetDistance(Square square1, Square square2)
-        {
-            return Math.Abs(square1.File - square2.File) + Math.Abs(square1.Rank - square2.Rank);
+            return recordEval;
         }
     }
 }
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,9 +1,7 @@
-﻿//#define LOGGING
+﻿#define LOG
 using ChessChallenge.API;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 
 public class MyBot : IChessBot
 {
@@ -14,115 +12,99 @@ public class MyBot : IChessBot
     private const ulong k_TpMask = 0x7FFFFF; //4.7 million entries, likely consuming about 151 MB
     private Transposition[] transposTable; // ref  m_TPTable[zHash & k_TpMask]
 
-    readonly int maxPieceSum;
+    int max_depth = 8;
+    int max_time = 0;
 
-    int m_depth = 4;
-    int maxMillisPerTurn;
+#if LOG
+    int cacheHits = 0;
+    int nodes = 0;
+    #endif
 
     public MyBot()
     {
         transposTable = new Transposition[k_TpMask + 1];
-        maxPieceSum = pieceValues[1] * 8 + pieceValues[2] * 2 + pieceValues[3] * 2 + pieceValues[4] * 2 + pieceValues[5] + pieceValues[6];
     }
 
     public Move Think(Board board, Timer timer)
     {
-        int pieces = BitboardHelper.GetNumberOfSetBits(board.AllPiecesBitboard);
-        if (pieces < 15) m_depth = 5;
-        else if (pieces <= 10) m_depth = 6;
-        else if (pieces <= 8) m_depth = 7;
-        else if (pieces <= 6) m_depth = 8;
-
-        maxMillisPerTurn = timer.MillisecondsRemaining / 10;
-
-        List<Move> bestMoves = new List<Move>();
-        int bestScore = int.MinValue;
-        foreach (Move move in board.GetLegalMoves().OrderByDescending(m => pieceValues[(int)m.MovePieceType])) {
-            board.MakeMove(move);
-            int score = CalcRecursive(board, timer, m_depth, 1);
-            board.UndoMove(move);
-
-            if (score > bestScore)
-            {
-                bestMoves.Clear();
-                bestScore = score;
-            }
-            if (score >= bestScore) bestMoves.Add(move);
-        }
-        return SelectBestMove(board, bestMoves);
-    }
-
-    int EvalMaterial(Board board)
-    {
-        return board.GetAllPieceLists().Chunk(6).Select(chunk => chunk.Sum(list => list.Sum(p => pieceValues[(int)p.PieceType]))).Aggregate((white, black) => white - black);
-    }
-
-    int CalcRecursive(Board board, Timer timer, int maxDepth, int currentDepth)
-    {
-        if (board.IsInCheckmate()) return currentDepth % 2 == 0 ? board.PlyCount - maxPieceSum : maxPieceSum - board.PlyCount;
-        if (board.IsInsufficientMaterial() || board.IsRepeatedPosition() || board.FiftyMoveCounter == 50 || board.IsDraw()) return 0;
-
-        Transposition transposition = transposTable[board.ZobristKey & k_TpMask];
-
-        if (transposition.zobristHash == board.ZobristKey /* && transposition.flag != INVALID && transposition.depth >= 0 */)
+        max_time = timer.MillisecondsRemaining / 20;
+        Transposition bestMove = transposTable[board.ZobristKey & k_TpMask];
+        for (sbyte i = 1; i <= max_depth; i++)
         {
+#if LOG
+            cacheHits = 0;
+            nodes = 0;
+#endif
+            CalcRecursive(board, i, board.IsWhiteToMove ? 1 : -1);
+            bestMove = transposTable[board.ZobristKey & k_TpMask];
+#if LOG
+            Console.WriteLine("Depth: {0,2} | Nodes: {1,10} | Time: {2,5} ms | Best {3} | Eval: {4}", i, nodes, timer.MillisecondsElapsedThisTurn, bestMove.move, bestMove.evaluation);
+#endif
+            if ((max_time - timer.MillisecondsElapsedThisTurn) < timer.MillisecondsElapsedThisTurn * Math.Pow(i+1, 3))
+            {
+                break;
+            }
+        }
+        return bestMove.move;
+    }
+
+    int CalcRecursive(Board board, sbyte currentDepth, int side)
+    {
+#if LOG
+        nodes++;
+#endif
+
+        ref Transposition transposition = ref transposTable[board.ZobristKey & k_TpMask];
+
+        if (transposition.zobristHash == board.ZobristKey && transposition.flag != INVALID && transposition.depth >= currentDepth)
+        {
+#if LOG
+            cacheHits++;
+#endif
             return transposition.evaluation;
         }
-        if (currentDepth >= maxDepth)
-        {
-            int boardScore = (currentDepth % 2 == 0) ^ board.IsWhiteToMove ? -EvalMaterial(board) : EvalMaterial(board);
-            transposition.evaluation = boardScore;
-            return boardScore;
-        }
 
+        if (board.IsInCheckmate()) return int.MinValue + board.PlyCount;
+        if (board.IsDraw()) return -10;
+        if (currentDepth <= 0) return EvalMaterial(board, side) * side;
+
+        Move[] moves = board.GetLegalMoves();
+        Move bestMove = moves[0];
         int bestScore = int.MinValue;
-        foreach (Move move in board.GetLegalMoves().OrderByDescending(m => pieceValues[(int)m.MovePieceType]))
+        foreach (Move move in moves.OrderByDescending(m => pieceValues[(int)m.MovePieceType]))
         {
             board.MakeMove(move);
-            int score = CalcRecursive(board, timer, maxDepth, currentDepth + 1);
+            int score = -CalcRecursive(board, (sbyte) (currentDepth - 1), -side);
             board.UndoMove(move);
-            if (score > bestScore) bestScore = score;
-            if (timer.MillisecondsElapsedThisTurn > maxMillisPerTurn) return bestScore;
+            if (score > bestScore)
+            {
+                bestScore = score;
+                bestMove = move;
+            }
         }
+
+        transposition.zobristHash = board.ZobristKey;
+        transposition.move = bestMove;
+        transposition.evaluation = bestScore;
+        transposition.flag = EXACT;
+        transposition.depth = currentDepth;
         return bestScore;
     }
 
-    Move SelectBestMove(Board board, List<Move> moves)
+    int EvalMaterial(Board board, int side)
     {
-        Move bestMove = moves[new Random().Next(moves.Count)];
-        int bestValue = int.MinValue;
-
-        foreach (Move move in moves)
-        {
-            Square kingSquare = board.GetKingSquare(!board.IsWhiteToMove);
-            int promotionValue = pieceValues[(int)move.PromotionPieceType];
-            int captureValue = pieceValues[(int)move.CapturePieceType];
-            int lostPieceValue = HighestLostPiece(board, move);
-            int progress = move.MovePieceType == PieceType.Pawn ? GetDistance(move.StartSquare, move.TargetSquare) : GetDistance(kingSquare, move.StartSquare) - GetDistance(kingSquare, move.TargetSquare);
-
-            int score = captureValue - lostPieceValue + promotionValue + progress;
-            if (score > bestValue)
-            {
-                bestMove = move;
-                bestValue = score;
-            }
-        }
-
-        return bestMove;
+        //Square kingSquare = board.GetKingSquare(side == 1);
+        int repetition = board.IsRepeatedPosition() ? 100 : 0;
+        int materialValue = board.GetAllPieceLists().Chunk(6).Select(chunk => chunk.Sum(list => list.Sum(p => pieceValues[(int)p.PieceType]))).Aggregate((white, black) => white - black);
+        return materialValue - repetition;
     }
 
-    int HighestLostPiece(Board board, Move move) {
-        board.MakeMove(move);
-        int maxValue = board.GetAllPieceLists().Chunk(6).ElementAt(board.IsWhiteToMove ? 1 : 0).SelectMany(list => list)
-            .Where(p => board.GetLegalMoves(true).Select(m => m.TargetSquare).Contains(p.Square)).Where(p => !board.SquareIsAttackedByOpponent(p.Square)).Select(p => pieceValues[(int)p.PieceType]).DefaultIfEmpty(0).Max();
-        board.UndoMove(move);
-        return maxValue;
-    }
-
+    /*
     int GetDistance(Square square1, Square square2)
     {
         return Math.Abs(square1.File - square2.File) + Math.Abs(square1.Rank - square2.Rank);
     }
+    */
 
     //14 bytes per entry, likely will align to 16 bytes due to padding (if it aligns to 32, recalculate max TP table size)
     public struct Transposition
